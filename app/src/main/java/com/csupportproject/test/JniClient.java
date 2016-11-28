eb45082eccb4702fdabae39f0af517b066c82b42
Magina <494109020@qq.com>
2016-11-28 17:48:40 +0800

    加点提示

--- app/src/main/java/com/csupportproject/test/JniClient.java
+++ app/src/main/java/com/csupportproject/test/JniClient.java
@@ -2,7 +2,8 @@ package com.csupportproject.test;
 
 /**
  * Created by Magina on 16/9/27.
- * 类功能介绍:
+ * 类功能介绍: jni使用的时候一定注意，包名什么的要和原文件所在包名完全一致。
+ * 因为native方法签名是和java方法的全限定包名＋方法名相关的。一旦变动过，就会导致方法找不到
  */
 
 public class JniClient {
