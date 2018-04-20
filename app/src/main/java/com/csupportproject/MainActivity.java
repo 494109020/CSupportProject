60e0d7f7ad633476e9b8948bd1bb8f99a9d71c51
Magina <494109020@qq.com>
2018-04-21 00:43:04 +0800

    test1

--- app/src/main/java/com/csupportproject/MainActivity.java
+++ app/src/main/java/com/csupportproject/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.currentTimeMillis();
 
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
